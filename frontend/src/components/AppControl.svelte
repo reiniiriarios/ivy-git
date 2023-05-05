8320a505ab0d4024b5576b4aa9e4d9c6c8e1ee1f
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2023-05-04 17:25:04 -0700

    feat: windows fluent ui icons for app controls

--- frontend/src/components/AppControl.svelte
+++ frontend/src/components/AppControl.svelte
@@ -4,8 +4,10 @@
   export let os: string = "";
   export let position: string;
 
+  let maximized = false;
+
   function minimize() {
-    (window as any).runtime.WindowIsMinimised().then((isMinimized: Boolean) => {
+    (window as any).runtime.WindowIsMinimised().then((isMinimized: boolean) => {
       isMinimized
         ? (window as any).runtime.WindowUnminimise()
         : (window as any).runtime.WindowMinimise();
@@ -13,10 +15,12 @@
   }
 
   function maximize() {
-    (window as any).runtime.WindowIsMaximised().then((isMaximized: Boolean) => {
+    (window as any).runtime.WindowIsMaximised().then((isMaximized: boolean) => {
+      console.log(isMaximized);
       isMaximized
         ? (window as any).runtime.WindowUnmaximise()
         : (window as any).runtime.WindowMaximise();
+      maximized = !isMaximized;
     });
   }
 
@@ -30,6 +34,32 @@
     <div class="control" id="close" on:click={close} on:keyup={close}><span>{@html octicons.x.toSVG({ "width": 13 })}</span></div>
     <div class="control" id="minimize" on:click={minimize} on:keyup={minimize}><span>{@html octicons.dash.toSVG({ "width": 13 })}</span></div>
     <div class="control" id="maximize" on:click={maximize} on:keyup={maximize}><span>{@html octicons.plus.toSVG({ "width": 13 })}</span></div>
+  {:else if os == 'windows'}
+    <div class="control" id="minimize" on:click={minimize} on:keyup={minimize}>
+      <span>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048">
+          <path d="M2048 819v205H0V819h2048z"></path>
+        </svg>
+      </span>
+    </div>
+    <div class="control" id="maximize" on:click={maximize} on:keyup={maximize}>
+      <span>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048">
+          {#if maximized}
+            <path d="M2048 1638h-410v410H0V410h410V0h1638v1638zM1434 614H205v1229h1229V614zm409-409H614v205h1024v1024h205V205z"></path>
+          {:else}
+            <path d="M2048 0v819h-205V350L350 1843h469v205H0v-819h205v469L1698 205h-469V0h819z"></path>
+          {/if}
+        </svg>
+      </span>
+    </div>
+    <div class="control" id="close" on:click={close} on:keyup={close}>
+      <span>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048">
+          <path d="M1169 1024l879 879-145 145-879-879-879 879L0 1903l879-879L0 145 145 0l879 879L1903 0l145 145-879 879z"></path>
+        </svg>
+      </span>
+    </div>
   {:else}
     <div class="control" id="minimize" on:click={minimize} on:keyup={minimize}><span>{@html octicons.dash.toSVG({ "width": 14 })}</span></div>
     <div class="control" id="maximize" on:click={maximize} on:keyup={maximize}><span>{@html octicons['screen-full'].toSVG({ "width": 13 })}</span></div>
@@ -103,6 +133,11 @@
         justify-content: center;
         align-items: center;
         fill: var(--color-scale-gray-4);
+
+        svg {
+          height: 10px;
+          width: 10px;
+        }
       }
 
       &:hover {
