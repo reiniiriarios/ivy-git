ad904092b5b096dc46bda73a511bc3b1844f1d97
Emma Litwa-Vulcu <emma@heysparkbox.com>
2023-05-12 10:47:09 -0700

    feat: keyboard navigation for commit rows

--- frontend/src/components/CommitRow.svelte
+++ frontend/src/components/CommitRow.svelte
@@ -15,7 +15,10 @@
     }
   }
 
-  function showDetails() {
+  function showDetails(e: MouseEvent | KeyboardEvent) {
+    if (e instanceof KeyboardEvent && ![' ', 'Enter'].includes(e.key)) {
+      return;
+    }
     clearActive();
     if ((window as any).currentCommitDetails() === commit.Hash) {
       (window as any).hideCommitDetails();
@@ -24,6 +27,20 @@
       (document.querySelector(`.commit[data-id="${commit.Id}"]`) as HTMLElement).classList.add('active');
     }
   }
+
+  function setKeyboard(el: HTMLElement) {
+    window.addEventListener('keydown', function(e: KeyboardEvent) {
+      if([' ', 'ArrowDown', 'ArrowUp'].includes(e.key) && e.target == el) {
+        e.preventDefault();
+        if (e.key === 'ArrowDown' && document.activeElement?.nextElementSibling instanceof HTMLElement) {
+          document.activeElement.nextElementSibling.focus();
+        }
+        else if (e.key === 'ArrowUp' && document.activeElement?.previousElementSibling instanceof HTMLElement) {
+          document.activeElement.previousElementSibling.focus();
+        }
+      }
+    });
+  }
 </script>
 
 <tr class="commit c-{commit.Color} {u ? 'uncommitted' : ''} {commit.Merge ? 'merge' : ''} {commit.Stash ? 'stash' : ''}"
@@ -31,8 +48,10 @@
   data-hash="{commit.Hash}"
   data-head="{h}"
   data-menu="{u ? '' : 'commit'}"
+  tabindex="0"
   on:click={showDetails}
-  on:keyup={showDetails}>
+  on:keyup={showDetails}
+  use:setKeyboard>
   <td class="commit__td commit__td--refs">
     {#if commit.Labeled}
       <CommitLabels commit={commit} HEAD={HEAD} />
