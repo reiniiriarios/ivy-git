5610fce112dc5375a09540b6087e10987a6a1463
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2023-12-11 18:14:25 -0800

    wip: deb pkg

---
+++ debian/ivy-git.doc-base.ex
@@ -0,0 +1,7 @@
+Document: ivy-git
+Title: Ivy Git Manual
+Author: Emma Litwa-Vulcu
+Abstract: Ivy Git aims to simplify workflow by putting the least amount of
+   resistance between the user and git as possible, enabling diverse
+   workflows without the constraints of being opinionated.
+Section: Git
---
+++ debian/manpage.md.ex
@@ -0,0 +1,27 @@
+% ivy-git(SECTION) | User Commands
+%
+% "December 11 2023"
+
+# NAME
+
+ivy-git - Git GUI
+
+# SYNOPSIS
+
+**ivy-git**
+
+# DESCRIPTION
+
+**ivy-git** is a Git GUI. More information at https://ivygit.com
+
+# SEE ALSO
+
+**git**(1)
+
+# AUTHOR
+
+Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
+
+# COPYRIGHT
+
+Copyright © 2023 Emma Litwa-Vulcu
